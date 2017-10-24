c434b4acb072dd2b67aed545df895a758d6a9442
Juho Hämäläinen <juho.hamalainen@jolla.com>
2017-10-24 14:05:20 +0300

    [common] Add new defines for volume sync.

---
+++ src/common/include/sailfishos/defines.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 Jolla Ltd.
+ *
+ * Contact: Juho Hämäläinen <juho.hamalainen@jolla.com>
+ *
+ * These PulseAudio Modules are free software; you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA.
+ */
+#ifndef _SAILFISHOS_DEFINES_H_
+#define _SAILFISHOS_DEFINES_H_
+
+#define PA_SAILFISHOS_MEDIA_VOLUME_SYNC                 "x-sailfishos.volume.sync"
+#define PA_SAILFISHOS_MEDIA_VOLUME_IN_SYNC              (0)
+#define PA_SAILFISHOS_MEDIA_VOLUME_CHANGE_DONE          (-1)
+#define PA_SAILFISHOS_MEDIA_VOLUME_CHANGING             (1)
+
+#endif
